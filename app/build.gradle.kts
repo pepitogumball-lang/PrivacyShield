feb68bef94a5d3a5c46514bf3110a74748d56a10
pepitogumball-lang <pepitogumball@gmail.com>
2026-05-18 17:54:11 -0700

    feat: persistence, permissions, Spanish locale, scan settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.privacyshield"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
